4cb47f54c544573f966b7871b08a9a5421183912
傅强 <qc_43ae1fa1932b4ee3a78bbac927627e79@git.code.tencent.com>
2019-06-25 16:28:03 +0800

    Initial commit

---
+++ src/main/java/com/vurtne/proproject/ProprojectApplication.java
@@ -0,0 +1,13 @@
+package com.vurtne.proproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProprojectApplication.class, args);
+	}
+
+}
