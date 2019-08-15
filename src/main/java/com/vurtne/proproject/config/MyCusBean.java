9a6a33205846f0a14c129997cd0a8fec71024a36
vurtne <330256579@qq.com>
2019-08-15 13:36:19 +0800

    modify callback url

--- src/main/java/com/vurtne/proproject/config/MyCusBean.java
+++ src/main/java/com/vurtne/proproject/config/MyCusBean.java
@@ -1,7 +1,11 @@
 package com.vurtne.proproject.config;
 
 import com.vurtne.proproject.utils.HttpUtil;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -18,5 +22,17 @@ public class MyCusBean {
     @Bean
     public HttpUtil httpUtil(){
         return new HttpUtil();
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(){
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedQueryChars","|{}[]");
+            }
+        });
+        return factory;
     }
 }
